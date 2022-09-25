84e7d9b1565fa8f9f739d0f5b8edd1db09c88a23
Jan Janiszewski <pole19901990@gmail.com>
2022-09-25 22:11:51 +0200

    asdf

--- Assignment 1/Jan_WorkingVersion.R
+++ Assignment 1/Jan_WorkingVersion.R
@@ -1,3 +1,6 @@
+#### WORKING VERSION (NOT UPDATED)
+
+
 # Libraries
 library(pacman)
 p_load(tidyverse)
@@ -15,6 +18,8 @@ print('Data is normal')
 n <- length(birthweight)
 mu <- mean(birthweight)
 sd <- sd(birthweight)
+t <- qt(p=.1/2, df=59, lower.tail=FALSE)
+
 ci <- 1.645 * sd / sqrt(n)
 
 lower <- mu - ci
@@ -25,7 +30,7 @@ print(lower)
 print(upper)
 
 #### 1c ####
-t.test(birthweight, mu=2800, alternative = "greater")
+t.test(birthweight, mu=2800, alternative = "greater", conf.level = 0.9)
 
 #### 1d ####
 
@@ -35,16 +40,18 @@ k = 140
 p = k/n
 q = 1-p
 
-#### 2b+c ####
+#### 2b ####
 p - (2.65*sqrt(p * q/n)) # 
 p + (2.65*sqrt(p * q/n))
 
 binom.test(140, n=200, p=0.7, conf.level=.99)
 
+#### 2c ####
+binom.test(140, n=200, p=0.75, conf.level=.99)
+
 #### 3a+b ####
 weather <- read.csv('weather.txt', sep='\t')
 
-weather
 plot(weather)
 hist(weather$humidity)
 hist(weather$temperature)
@@ -60,7 +67,11 @@ summary(weather)
 n <- length(weather$humidity)
 mu <- mean(weather$temperature)
 sd <- sd(weather$temperature)
-ci <- 1.645 * sd
+t <- qt(p=.1/2, df=60, lower.tail=FALSE)
+
+ci <- t * sd / sqrt(n)
+
+t.test(birthweight, mu=2800, alternative = "greater", conf.level = 0.9)
 
 lower <- mu - ci
 upper <- mu + ci
@@ -76,6 +87,15 @@ TBD
 #### 4b ####
 austen <- read.csv(file = 'austen.txt', sep = '\t')
 
-chisq.test(austen)
+read.table('austen.txt', header=TRUE)
+
+austen_self <- austen %>% select(-Sand2)
+cs_self <- chisq.test(austen_self)
+cs_self
 
 #### 4c ####
+austen_other <- austen %>% mutate(Austen = Sense + Emma + Sand1)
+
+cs_other <- chisq.test(austen_other %>% select(Sand2, Austen))
+cs_other
+
